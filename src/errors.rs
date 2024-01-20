f10acc68c888375a18c1350c83bfc43c5abaa2c9
Vikyath Shirva <vicky.fames@gmail.com>
2024-01-20 19:08:37 +0530

    D2: added error handling and refactored a few lines

---
+++ src/errors.rs
@@ -0,0 +1,54 @@
+use std::fmt;
+use std::process::exit;
+// Allows to display a variant with the format {:?}.
+#[derive(Debug)]
+pub enum Errcode { // contains all possible error codes in the tool.
+
+    ArgumentInvalid(& 'static str)
+
+}
+
+impl Errcode{
+    pub fn get_retcode(&self) -> i32 {
+        1
+    }
+}
+
+
+#[allow(unreachable_patterns)]
+impl fmt::Display for Errcode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Errcode::ArgumentInvalid(element) => write!(f, "ArgumentInvalid: {}", element),
+            _ => write!(f, "{:?}", self)
+        }
+    }
+}
+
+
+
+
+pub fn exit_with_retcode(res: Result<(), Errcode>) {
+    match res {
+        Ok(_) => {
+            log::debug!("Exit without any error, returning 0");
+            exit(0);
+        },
+
+    Err(e) => {
+        let retcode = e.get_retcode();
+        log::error!("Error on exit: \n\t{}\n\tReturning {}", e, retcode);
+        exit(retcode);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod cli;
+mod errors;
+use errors::exit_with_retcode;
+use dotenv::dotenv;
+use std::process::exit;
+
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    match cli::parse_args(){
+        Ok(args) => {
+            log::info!("{:?}", args);
+            exit_with_retcode(Ok(()))
+        },
+        Err(e) => {
+            log::error!("Error while passing arguments:\n\t{}", e);
+            exit(e.get_retcode());
+        }
+    };
 }
